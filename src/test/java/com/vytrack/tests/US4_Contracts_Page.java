e3d97dfe873e153a5a0f70f630cbdbbd4c75f1e0
Nawar Hussein <nawar_hussien2000@yahoo.com>
2022-03-07 00:53:23 -0500

    new user story

--- src/test/java/com/vytrack/tests/US4_Contracts_Page.java
+++ src/test/java/com/vytrack/tests/US4_Contracts_Page.java
@@ -1,17 +1,31 @@
 package com.vytrack.tests;
 
+import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class US4_Contracts_Page {
-    @Test
-    public void SalesManager() throws InterruptedException {
 
-        Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
+@AfterMethod
+ public void terminate(){
+   BrowserUtils.sleep(3);
+     Driver.getDriver().close();
+   }
+   @BeforeMethod
+   public void start(){
+       Driver.getDriver().get("https://qa1.vytrack.com/user/login");
+
+   }
+
+    @Test
+    public void SalesManager(){
+
+       // Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
 
         WebElement userName = Driver.getDriver().findElement(By.xpath("//input[@name='_username']"));
         userName.sendKeys(ConfigurationReader.getProperty("sales_manager_username"));
@@ -22,17 +36,37 @@ public class US4_Contracts_Page {
         WebElement login = Driver.getDriver().findElement(By.xpath("//button[@type='submit']"));
         login.click();
 
+        BrowserUtils.sleep(7);
+
         WebElement fleetButton = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[2]"));
         fleetButton.click();
-
-        Thread.sleep(2000);
 
         WebElement vehicalContrat = Driver.getDriver().findElement(By.xpath("//span[.='Vehicle Contracts']"));
         vehicalContrat.click();
     }
 
-    @AfterMethod
-    public void terminate(){
-        Driver.getDriver().close();
+    @Test
+    public void StoreManager(){
+
+            // Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
+
+            WebElement userName = Driver.getDriver().findElement(By.xpath("//input[@name='_username']"));
+            userName.sendKeys(ConfigurationReader.getProperty("store_manager_username"));
+
+            WebElement password = Driver.getDriver().findElement(By.xpath("//input[@name='_password']"));
+            password.sendKeys(ConfigurationReader.getProperty("sales_manager_password"));
+
+            WebElement login = Driver.getDriver().findElement(By.xpath("//button[@type='submit']"));
+            login.click();
+
+            BrowserUtils.sleep(7);
+
+            WebElement fleetButton = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[2]"));
+            fleetButton.click();
+
+            WebElement vehicalContrat = Driver.getDriver().findElement(By.xpath("//span[.='Vehicle Contracts']"));
+            vehicalContrat.click();
     }
+
+
 }
