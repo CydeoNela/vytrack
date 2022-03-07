8b024716409549c9c10919b5c17f495aee9aa7dc
Nawar Hussein <nawar_hussien2000@yahoo.com>
2022-03-06 23:47:32 -0500

    new user story

--- src/test/java/com/vytrack/tests/US4_Contracts_Page.java
+++ src/test/java/com/vytrack/tests/US4_Contracts_Page.java
@@ -1,4 +1,38 @@
 package com.vytrack.tests;
 
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
 public class US4_Contracts_Page {
+    @Test
+    public void SalesManager() throws InterruptedException {
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
+
+        WebElement userName = Driver.getDriver().findElement(By.xpath("//input[@name='_username']"));
+        userName.sendKeys(ConfigurationReader.getProperty("sales_manager_username"));
+
+        WebElement password = Driver.getDriver().findElement(By.xpath("//input[@name='_password']"));
+        password.sendKeys(ConfigurationReader.getProperty("sales_manager_password"));
+
+        WebElement login = Driver.getDriver().findElement(By.xpath("//button[@type='submit']"));
+        login.click();
+
+        WebElement fleetButton = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[2]"));
+        fleetButton.click();
+
+        Thread.sleep(2000);
+
+        WebElement vehicalContrat = Driver.getDriver().findElement(By.xpath("//span[.='Vehicle Contracts']"));
+        vehicalContrat.click();
+    }
+
+    @AfterMethod
+    public void terminate(){
+        Driver.getDriver().close();
+    }
 }
