3e91c9cea39d931392a31712835635373381074f
Nawar Hussein <nawar_hussien2000@yahoo.com>
2022-03-07 21:32:19 -0500

    new user story updated

--- src/test/java/com/vytrack/tests/US4_Contracts_Page.java
+++ src/test/java/com/vytrack/tests/US4_Contracts_Page.java
@@ -1,31 +1,22 @@
 package com.vytrack.tests;
 
+import com.vytrack.tests.base.TestBase;
 import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class US4_Contracts_Page {
+public class US4_Contracts_Page extends TestBase {
 
-@AfterMethod
- public void terminate(){
-   BrowserUtils.sleep(3);
-     Driver.getDriver().close();
-   }
-   @BeforeMethod
-   public void start(){
-       Driver.getDriver().get("https://qa1.vytrack.com/user/login");
 
-   }
 
     @Test
-    public void SalesManager(){
-
-       // Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
+    public void SalesManager() {
 
         WebElement userName = Driver.getDriver().findElement(By.xpath("//input[@name='_username']"));
         userName.sendKeys(ConfigurationReader.getProperty("sales_manager_username"));
@@ -36,36 +27,48 @@ public class US4_Contracts_Page {
         WebElement login = Driver.getDriver().findElement(By.xpath("//button[@type='submit']"));
         login.click();
 
-        BrowserUtils.sleep(7);
+       BrowserUtils.sleep(7);
 
         WebElement fleetButton = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[2]"));
         fleetButton.click();
 
-        WebElement vehicalContrat = Driver.getDriver().findElement(By.xpath("//span[.='Vehicle Contracts']"));
-        vehicalContrat.click();
+        WebElement vehicleContractButton = Driver.getDriver().findElement(By.xpath("//span[.='Vehicle Contracts']"));
+        vehicleContractButton.click();
+
+        BrowserUtils.sleep(5);
+
+
+        WebElement vehicleContractPage = Driver.getDriver().findElement(By.xpath("//h1[@class='oro-subtitle']"));
+        Assert.assertTrue(vehicleContractPage.isDisplayed());
+
+
     }
 
     @Test
-    public void StoreManager(){
+    public void StoreManager() {
 
-            // Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
 
-            WebElement userName = Driver.getDriver().findElement(By.xpath("//input[@name='_username']"));
-            userName.sendKeys(ConfigurationReader.getProperty("store_manager_username"));
+        WebElement userName = Driver.getDriver().findElement(By.xpath("//input[@name='_username']"));
+        userName.sendKeys(ConfigurationReader.getProperty("store_manager_username"));
 
-            WebElement password = Driver.getDriver().findElement(By.xpath("//input[@name='_password']"));
-            password.sendKeys(ConfigurationReader.getProperty("sales_manager_password"));
+        WebElement password = Driver.getDriver().findElement(By.xpath("//input[@name='_password']"));
+        password.sendKeys(ConfigurationReader.getProperty("sales_manager_password"));
 
-            WebElement login = Driver.getDriver().findElement(By.xpath("//button[@type='submit']"));
-            login.click();
+        WebElement login = Driver.getDriver().findElement(By.xpath("//button[@type='submit']"));
+        login.click();
 
-            BrowserUtils.sleep(7);
+       BrowserUtils.sleep(7);
+        WebElement fleetButton = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[2]"));
+        fleetButton.click();
 
-            WebElement fleetButton = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[2]"));
-            fleetButton.click();
+        WebElement vehicleContractButton = Driver.getDriver().findElement(By.xpath("//span[.='Vehicle Contracts']"));
+        vehicleContractButton.click();
 
-            WebElement vehicalContrat = Driver.getDriver().findElement(By.xpath("//span[.='Vehicle Contracts']"));
-            vehicalContrat.click();
+        BrowserUtils.sleep(5);
+
+
+        WebElement vehicleContractPage = Driver.getDriver().findElement(By.xpath("//h1[@class='oro-subtitle']"));
+        Assert.assertTrue(vehicleContractPage.isDisplayed());
     }
 
 
