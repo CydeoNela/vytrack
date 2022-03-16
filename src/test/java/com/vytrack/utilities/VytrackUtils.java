777a32c2d4dba21ab213a087eb270020273da1a4
Daniela <nelavip@yahoo.com>
2022-03-15 21:19:13 -0700

    VyTrackUtils updated

--- src/test/java/com/vytrack/utilities/VytrackUtils.java
+++ src/test/java/com/vytrack/utilities/VytrackUtils.java
@@ -1,11 +1,6 @@
 package com.vytrack.utilities;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class VytrackUtils {
 
@@ -29,19 +24,19 @@ public class VytrackUtils {
         // Driver.getDriver().findElement(By.cssSelector("#prependedInput"));
         Driver.getDriver().findElement(By.id("prependedInput")).sendKeys(ConfigurationReader.getProperty("driver_username"));
         //pass password
-        Driver.getDriver().findElement(By.cssSelector("#prependedInput2")).sendKeys(ConfigurationReader.getProperty("password"));
+        Driver.getDriver().findElement(By.cssSelector("#prependedInput2")).sendKeys(ConfigurationReader.getProperty("driver_password"));
         //click login button
         Driver.getDriver().findElement(By.tagName("button")).click();
     }
 
-    public static void loginAsStoreManager() {
+    public static void loginAsStoreManger() {
         //go to website
         Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
         // pass username
         // Driver.getDriver().findElement(By.cssSelector("#prependedInput"));
         Driver.getDriver().findElement(By.id("prependedInput")).sendKeys(ConfigurationReader.getProperty("store_manager_username"));
         //pass password
-        Driver.getDriver().findElement(By.cssSelector("#prependedInput2")).sendKeys(ConfigurationReader.getProperty("password"));
+        Driver.getDriver().findElement(By.cssSelector("#prependedInput2")).sendKeys(ConfigurationReader.getProperty("store_manager_password"));
         //click login button
         Driver.getDriver().findElement(By.tagName("button")).click();
     }
@@ -53,22 +48,10 @@ public class VytrackUtils {
         // Driver.getDriver().findElement(By.cssSelector("#prependedInput"));
         Driver.getDriver().findElement(By.id("prependedInput")).sendKeys(ConfigurationReader.getProperty("sales_manager_username"));
         //pass password
-        Driver.getDriver().findElement(By.cssSelector("#prependedInput2")).sendKeys(ConfigurationReader.getProperty("password"));
+        Driver.getDriver().findElement(By.cssSelector("#prependedInput2")).sendKeys(ConfigurationReader.getProperty("sales_manager_password"));
         //click login button
         Driver.getDriver().findElement(By.tagName("button")).click();
     }
 
-    public static void waitTillLoaderMaskDisappear() {
-        try {
-            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
-            WebElement loaderMask = Driver.getDriver().findElement(By.cssSelector("div[class='loader-mask shown']"));
-            wait.until(ExpectedConditions.invisibilityOf(loaderMask));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+
 }
-
-
-  
-
